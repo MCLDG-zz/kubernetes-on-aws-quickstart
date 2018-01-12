381a2a567ce6ec59bdd8164f0c0ba9b8f6e45065
Michael Edge <edgema@c4b301d07c5d.ant.amazon.com>
2018-01-12 18:52:26 +0800

    added cluster scaling test cases

--- cluster-scaling/quick-start.sh
+++ cluster-scaling/quick-start.sh
@@ -7,12 +7,13 @@ function getprop {
 
 region=$(getprop 'aws.region')
 clustername=$(getprop 'k8s.clustername').cluster.k8s.local
+asgname=nodes.${clustername}
 echo your region is ${region}
 echo your cluster name is ${clustername}
 
 #Scaling - check the size of your worker node auto scaling group
 echo checking the size of your worker node auto scaling group
-maxsize=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names nodes.${clustername} --region $region | jq .AutoScalingGroups[0].MaxSize`
+maxsize=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq .AutoScalingGroups[0].MaxSize`
 
 #this checks the size of the ASG. It should be equivalent to check the maxsize in 'kops edit ig nodes'
 if [ $maxsize -eq 2 ]; then
@@ -26,12 +27,11 @@ if [ $maxsize -eq 2 ]; then
     kops replace -f ignodesupdated.yaml
     kops update cluster ${clustername} --yes
     kops rolling-update cluster ${clustername}
+    rm ignodes.yaml
+    rm ignodesupdated.yaml
 else
     echo your worker nodes MaxSize is $maxsize, which is fine. No need to update MaxSize
 fi
-
-rm ignodes.yaml
-rm ignodesupdated.yaml
 
 #update the K8s cluster with the autoscaling IAM policy. Note that adding the policy more than once has no effect,
 #so this can be rerun against clusters that already include this policy
@@ -44,6 +44,24 @@ kops replace -f ${config}
 kops update cluster ${clustername} --yes
 kops rolling-update cluster ${clustername}
 rm ${config}
+
+#update and apply the autoscaling yaml config
+echo updating the autoscaling yaml config
+sed "s/<NODE ASG>/$asgname/g" ./cluster-scaling/templates/2-10-autoscaler.yaml | \
+sed "s/<REGION>/$region/g" > ./cluster-scaling/templates/temp-2-10-autoscaler.yaml
+kubectl apply -f ./cluster-scaling/templates/temp-2-10-autoscaler.yaml
+
+#check autoscaling service has started
+while true; do
+    if kubectl logs deployment/cluster-autoscaler --namespace=kube-system | grep -q 'successfully renewed lease'; then
+        echo Autoscaling service has started
+        break
+    else
+        echo Autoscaling service startup in progress. This may take a few seconds...
+        sleep 5
+        continue
+    fi
+done
 
 #check that the scaling component is working correctly
 ./cluster-scaling/test.sh
--- cluster-scaling/test.sh
+++ cluster-scaling/test.sh
@@ -1,1 +1,43 @@
 #!/usr/bin/env bash
+
+#function to read properties from quick-start.properties
+function getprop {
+    grep "${1}" quick-start.properties|cut -d'=' -f2
+}
+
+region=$(getprop 'aws.region')
+clustername=$(getprop 'k8s.clustername').cluster.k8s.local
+asgname=nodes.${clustername}
+echo your region is ${region}
+echo your cluster name is ${clustername}
+
+#Scaling - check the desired size of your worker node auto scaling group
+beforedesiredsize=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq .AutoScalingGroups[0].DesiredCapacity`
+echo size of your worker node auto scaling group before scaling is: $beforedesiredsize
+
+#deploy a pod that will use more capacity than is available. This will cause the cluster worker nodes to scale
+echo deploying a pod that will use more capacity than is available. This will cause the cluster worker nodes to scale
+kubectl apply -f ./cluster-scaling/templates/dummy-resource-offers.yaml
+
+#Scaling - check the desired size of your worker node auto scaling group
+afterdesiredsize=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq '.AutoScalingGroups[0].DesiredCapacity'`
+echo size of your worker node auto scaling group after scaling is: $afterdesiredsize
+
+#this checks the size of the ASG. It should be equivalent to check the maxsize in 'kops edit ig nodes'
+while [ $beforedesiredsize -eq $afterdesiredsize ]; do
+    echo the worker nodes ASG has not yet scaled. Waiting for it to scale out...
+    afterdesiredsize=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq '.AutoScalingGroups[0].DesiredCapacity'`
+    sleep 10
+done
+
+#here we check that the number of instances matches the desired size of the ASG
+noinstances=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq '.AutoScalingGroups[0].Instances | length'`
+echo number of worker node instances in auto scaling group after scaling is: $noinstances
+
+while [ $noinstances -ne $afterdesiredsize ]; do
+    noinstances=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $asgname --region $region | jq '.AutoScalingGroups[0].Instances | length'`
+    echo number of worker node instances in auto scaling group after scaling is: $noinstances
+    echo number of EC2 in the ASG does NOT matches the desired size. Waiting for ASG to scale out...
+done
+
+echo the worker nodes ASG has scaled. Auto-scaling is working.
